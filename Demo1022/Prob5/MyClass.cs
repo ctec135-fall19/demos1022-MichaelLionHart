64dc1c2b7265c3d23665f1d2870dffe0a173c187
Michael Hart <M.HART@students.clark.edu>
2019-10-22 15:08:58 -0700

    Tuesday 10/22/19 demos

---
+++ Demo1022/Prob5/MyClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    // create a class with two fields, one a string and the other an int.
+    // the class implements the IComparable interface.
+    class MyClass
+    {
+
+        // fields
+        private string mcStr;
+
+        // properties to access private members
+
+
+        public int CompareTo(object obj)
+        {
+            // cast obj to MyClass type
+            MyClass mc = obj as MyClass;
+
+            // if cast successful make comparisons
+            if (mc != null)
+            {
+                // return value corresponding to comparison
+                if (String.Compare(this.mcStr, mc.mcStr) < 0) return -1;
+                if (String.Compare(this.mcStr, mc.mcStr) > 0) return 1;
+                else
+                {
+                    if (this.McInt < mc.McInt) return -1;
+                    if (this.McInt > mc.McInt) return 1;
+                    else return 0;
+                }
+            }
+            else
+                throw new ArgumentException("Parameter is not a MyClass object");
+        }
+
+        // override ToString so can print object
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Demo1022/Prob5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create some objects to use for testing
+
+        }
+    }
+}
